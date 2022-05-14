38d157d7de7f785b7e269dc8e2d31527c3247bc4
akikr <ankit.kumar962@gmail.com>
2022-05-14 19:05:32 +0530

    Modified: The BookController to render data if user is logged-in & also modified the book view html to get user's book info and later render it if user is logged-in

--- src/main/java/io/akikr/betterreads/controller/BookController.java
+++ src/main/java/io/akikr/betterreads/controller/BookController.java
@@ -1,20 +1,27 @@
 package io.akikr.betterreads.controller;
 
 import io.akikr.betterreads.models.Book;
+import io.akikr.betterreads.models.UserBooks;
+import io.akikr.betterreads.models.UserBooksPrimaryKey;
 import io.akikr.betterreads.repository.BookRepository;
+import io.akikr.betterreads.repository.UserBooksRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-import java.util.Objects;
 import java.util.Optional;
+
+import static java.util.Objects.nonNull;
 
 /**
  * @apiNote : The BookController to get book data from cassandra database
  * @author ankit
+ * @since 1.0
  */
 
 @Controller
@@ -24,15 +31,17 @@ public class BookController
     private String coverImageBaseUrl;
 
     private final BookRepository bookRepository;
+    private final UserBooksRepository userBooksRepository;
 
     @Autowired
-    public BookController(BookRepository bookRepository)
+    public BookController(BookRepository bookRepository, UserBooksRepository userBooksRepository)
     {
         this.bookRepository = bookRepository;
+        this.userBooksRepository = userBooksRepository;
     }
 
     @GetMapping(value = "/books/{bookId}")
-    public String getBook(@PathVariable String bookId, Model model)
+    public String getBook(@PathVariable String bookId, Model model, @AuthenticationPrincipal OAuth2User principal)
     {
         Optional<Book> optionalBook = bookRepository.findById(bookId);
 
@@ -41,11 +50,29 @@ public class BookController
             Book book = optionalBook.get();
             String coverImageUrl = "/images/no-image.png";
 
-            if (Objects.nonNull(book.getCoverIds()) && book.getCoverIds().size() > 0)
+            if (nonNull(book.getCoverIds()) && book.getCoverIds().size() > 0)
                 coverImageUrl = coverImageBaseUrl + book.getCoverIds().get(0) + "-L.jpg";
 
             model.addAttribute("coverImage", coverImageUrl);
             model.addAttribute("book", book);
+
+            // Only if user is logged-in
+            if (nonNull(principal) && nonNull(principal.getAttribute("login")))
+            {
+                String userId = principal.getAttribute("login");
+                model.addAttribute("loginId", userId);
+
+                UserBooksPrimaryKey key = new UserBooksPrimaryKey();
+                key.setBookId(bookId);
+                key.setUserId(userId);
+                Optional<UserBooks> userBooks = userBooksRepository.findById(key);
+
+                if (userBooks.isPresent())
+                    model.addAttribute("userBooks", userBooks.get());
+                else
+                    model.addAttribute("userBooks", new UserBooks());
+            }
+
             return "book";
         }
         return "book-not-found";
